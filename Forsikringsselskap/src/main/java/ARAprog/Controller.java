ab883713606911a60eea2e9e4917d4d7a2cb1e84
S1rShadez <arthure97@hotmail.com>
2019-04-02 15:31:17 +0200

    Basic main files
    All the base files are here, use this for start of project

---
+++ Forsikringsselskap/src/main/java/ARAprog/Controller.java
@@ -0,0 +1,4 @@
+package ARAprog;
+
+public class Controller {
+}
---
+++ Forsikringsselskap/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module Forsikringsselskap {
+
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+
+    exports ARAprog;
+
+}
