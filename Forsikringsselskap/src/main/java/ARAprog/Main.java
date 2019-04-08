0989c8352d1d83177d983f61edb8b8ca8d7df462
S1rShadez <arthure97@hotmail.com>
2019-04-08 16:15:55 +0200

    Started work on abstractRead

--- Forsikringsselskap/src/main/java/ARAprog/Main.java
+++ Forsikringsselskap/src/main/java/ARAprog/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/FXML/GUI.fxml"));
-        primaryStage.setTitle("Forsikringsselskap");
+        primaryStage.setTitle("ARA Forsikring");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
--- Forsikringsselskap/src/main/java/ARAprog/ReadFile/AbstractRead.java
+++ Forsikringsselskap/src/main/java/ARAprog/ReadFile/AbstractRead.java
@@ -1,6 +1,28 @@
 package ARAprog.ReadFile;
 
+import java.io.*;
+import java.util.ArrayList;
+
 abstract class AbstractRead {
+
+    protected static ArrayList<String> readCSV(File file){
+        BufferedReader bufferedReader = null;
+        ArrayList<String> dataFromFile = new ArrayList<>();
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file.getAbsolutePath()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            dataFromFile.add(bufferedReader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return dataFromFile;
+    }
 
 
    /* public void chooseFile(){
