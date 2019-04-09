eef46017224a5c440a3d08a869384da3de5a8d16
S1rShadez <arthure97@hotmail.com>
2019-04-09 14:49:20 +0200

    Made writeCSV work

--- Forsikringsselskap/src/main/java/ARAprog/WriteFile/AbstractWrite.java
+++ Forsikringsselskap/src/main/java/ARAprog/WriteFile/AbstractWrite.java
@@ -1,4 +1,26 @@
 package ARAprog.WriteFile;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 abstract class AbstractWrite {
+
+    protected static void writeCSV(ArrayList<String> output, File path) throws IOException {
+        String newString;
+        FileWriter fw = new FileWriter(path);
+
+        for (String string : output){
+            if (string.contains(";")){
+                newString = string + "\n";
+                fw.write(newString);
+            }
+        }
+
+        fw.flush();
+        fw.close();
+
+    }
+
 }
--- Forsikringsselskap/src/main/java/ARAprog/WriteFile/WriteCSV.java
+++ Forsikringsselskap/src/main/java/ARAprog/WriteFile/WriteCSV.java
@@ -1,4 +1,19 @@
 package ARAprog.WriteFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class WriteCSV extends AbstractWrite {
+
+    public WriteCSV(ArrayList<String> dataOut, File path){
+
+        try {
+            writeCSV(dataOut, path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
