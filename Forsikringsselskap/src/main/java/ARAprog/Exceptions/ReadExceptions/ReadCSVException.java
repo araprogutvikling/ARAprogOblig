b0a27276516b5332c3ab6ceecabce02c4ecefa05
S1rShadez <arthure97@hotmail.com>
2019-04-11 12:08:07 +0200

    Fiksa masse error
    Fiksa masse error så prosjektet bær fungere nå! Da er det bare å jobbe videre

--- Forsikringsselskap/src/main/java/ARAprog/Exceptions/ReadExceptions/ReadCSVException.java
+++
@@ -1,7 +0,0 @@
-package ARAprog.Exceptions.ReadExceptions;
-
-public class ReadCSVException extends ReadException {
-    public ReadCSVException(String cause){
-        super(cause);
-    }
-}
--- Forsikringsselskap/src/main/java/ARAprog/Persons/PreviousCustomer.java
+++ Forsikringsselskap/src/main/java/ARAprog/Persons/PreviousCustomer.java
@@ -3,5 +3,7 @@ package ARAprog.Persons;
 public class PreviousCustomer extends Customer {
 
 
-
+    public PreviousCustomer(int pPnr, int pAge, String pFirstName, String pLastname, String pAdress, String pAreacode, String pPostalarea) {
+        super(pPnr, pAge, pFirstName, pLastname, pAdress, pAreacode, pPostalarea);
+    }
 }
