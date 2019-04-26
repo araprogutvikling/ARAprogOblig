8f84a31d4b044afd27a39538eb69b85a01d1201a
Rawdog1337 <48521275+Rawdog1337@users.noreply.github.com>
2019-04-26 14:01:49 +0200

    merge

--- Forsikringsselskap/src/main/java/ARAprog/Main.java
+++ Forsikringsselskap/src/main/java/ARAprog/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/FXML/GUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("resources/FXML/GUI.fxml"));
         primaryStage.setTitle("ARA Forsikring");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
