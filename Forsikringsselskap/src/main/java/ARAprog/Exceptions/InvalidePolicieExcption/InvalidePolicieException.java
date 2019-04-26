a044d182cbba85487b608cc066cd8828f31fab68
Rawdog1337 <48521275+Rawdog1337@users.noreply.github.com>
2019-04-26 14:08:27 +0200

    merge

---
+++ Forsikringsselskap/src/main/java/ARAprog/Exceptions/InvalidePolicieExcption/InvalidePolicieException.java
@@ -0,0 +1,8 @@
+package ARAprog.Exceptions.InvalidePolicieExcption;
+
+public class InvalidePolicieException extends Exception {
+
+    public InvalidePolicieException(String reason){
+        super(reason);
+    }
+}
