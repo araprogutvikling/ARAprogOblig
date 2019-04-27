4a19fa2471fe4dc2e5ef480cc0f1f95ab5f3f369
S1rShadez <arthure97@hotmail.com>
2019-04-27 15:57:14 +0200

    merging

--- Forsikringsselskap/src/main/java/ARAprog/Controller.java
+++ Forsikringsselskap/src/main/java/ARAprog/Controller.java
@@ -1,77 +1,8 @@
 package ARAprog;
 
 
-import ARAprog.ReadFile.ReadCSV;
-import ARAprog.ReadFile.ReadJOBJ;
-import javafx.concurrent.Service;
-import javafx.concurrent.Task;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.text.Text;
-import javafx.stage.FileChooser;
-import javafx.stage.PopupWindow;
-import javafx.stage.Window;
-
-import java.io.File;
-import java.util.ArrayList;
-
 public class Controller {
-    @FXML
-    public Button btnReadFile;
-    public ListView ScrollList;
-    Service<Void> dataLoaderThread;
 
-    public void readFile() {
-        //TODO: Make sure this works
-        FileChooser fileChooser = new FileChooser();
-        ArrayList<String> data = new ArrayList<>();
-        ScrollList.getItems().clear();
-        btnReadFile.setDisable(true);
-
-        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Excel csv-file", "*.csv"), new FileChooser.ExtensionFilter("Java object", "*.jobj"));
-        File selectedFile = fileChooser.showOpenDialog(new PopupWindow() {
-            @Override
-            public void show(Window window) {
-                super.show(window);
-            }
-        });
-        String fileType = fileChooser.getSelectedExtensionFilter().getDescription();
-        dataLoaderThread = new Service<Void>() {
-            @Override
-            protected Task<Void> createTask() {
-                return new Task<Void>() {
-                    @Override
-                    protected Void call() throws Exception {
-
-                        Thread.sleep(5000);
-
-                        if (fileType == "Excel csv-file") {
-                            ReadCSV read = new ReadCSV(selectedFile);
-                            data.addAll(read.getFileData());
-                            System.out.println(data);
-                            for (String text : data) {
-                                Text string = new Text(text + "\n");
-                                ScrollList.getItems().add(string);
-                            }
-
-                        } else if (fileType == "Java object") {
-                            ReadJOBJ read = new ReadJOBJ(selectedFile);
-
-                        } else {
-                            //TODO: Make a better error
-                            System.err.println("Wrong filetype!");
-
-                        }
-
-                        return null;
-                    }
-                };
-            }
-        };
-        dataLoaderThread.setOnSucceeded(event -> btnReadFile.setDisable(false));
-        dataLoaderThread.restart();
-    }
 
 
 
