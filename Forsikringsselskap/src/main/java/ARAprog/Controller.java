ab26bad8d39561f021ffbccf6df561b11e1e3791
S1rShadez <arthure97@hotmail.com>
2019-04-30 01:35:39 +0200

    Fixed more of the GUI tabs

--- Forsikringsselskap/src/main/java/ARAprog/Controller.java
+++ Forsikringsselskap/src/main/java/ARAprog/Controller.java
@@ -42,7 +42,7 @@ public class Controller{
     public ListView ScrollList;
     public Label lblOCustomer, lblOAddress, lblOCustomerStart, lblOYN1, lblOYN2, lblOYN3, lblOYN4, lblHInsuranceNr1,
             lblHInsurancePrm1, lblHAddress1, lblHType1, lblHBuildingMaterial1, lblHStandard1, lblHSquaremeter1,
-            lblHInsuranceVB1, lblHDateForInsurance1, lblHAmountInsurance1, lblHInsuranceVC11, lblBInsuranceNr1, lblBInsurancePrm1,
+            lblHInsuranceVB1, lblHDateForInsurance1, lblHAmountInsurance1, lblBInsuranceNr1, lblBInsurancePrm1,
             lblBOwner1, lblHType11, lblBTypeModel1, lblBRegNr1, lblBYear1, lblBMotor1, lblBAmountInsurance1,
             lblBDateForInsurance2, lblTInsuranceNr1, lblTInsurancePrm1, lblTDateForInsurance1, lblTAmountInsurance1, lblTSumInsurance1,
             lblAreaInsurance1, lblCInsuranceNr1, lblCInsurancePrm1, lblCAddress1, lblCType1, lblCBuildingMaterial1, lblHInsuranceVC1,
@@ -137,7 +137,6 @@ public class Controller{
         lblHInsuranceVB1.setText("----------");
         lblHDateForInsurance1.setText("----------");
         lblHAmountInsurance1.setText("----------");
-        lblHInsuranceVC11.setText("----------");
         lblBInsuranceNr1.setText("----------");
         lblBInsurancePrm1.setText("----------");
         lblBOwner1.setText("----------");
@@ -171,7 +170,7 @@ public class Controller{
     public void getSelectedCustomer(){
         int selectedCustomer = ScrollList.getSelectionModel().getSelectedIndex();
         String[] selectedCustomerData = parseCustomerData(data.get(selectedCustomer));
-
+        //"Oversikt" tab'en
         lblOCustomer.setText("Kundenr: " + selectedCustomerData[0] + " | Navn: " + selectedCustomerData[1] + " " + selectedCustomerData[2] + " | Personnr: " + selectedCustomerData[3]);
         lblOAddress.setText("Faktureringsadresse: " + selectedCustomerData[4] + ", " + selectedCustomerData[5] + " " + selectedCustomerData[6]);
         lblOCustomerStart.setText("Kundeforhold opprettet: " + selectedCustomerData[7]);
@@ -179,6 +178,39 @@ public class Controller{
         lblOYN2.setText(selectedCustomerData[9]);
         lblOYN3.setText(selectedCustomerData[10]);
         lblOYN4.setText(selectedCustomerData[11]);
+
+        //"Hus og innbo" tab'en
+        lblHInsuranceNr1.setText(selectedCustomerData[12]);
+        lblHInsurancePrm1.setText(selectedCustomerData[13]);
+        lblHDateForInsurance1.setText(selectedCustomerData[14]);
+        lblHAmountInsurance1.setText(selectedCustomerData[15]);
+        lblHAddress1.setText(selectedCustomerData[16] + ", " + selectedCustomerData[17] + " " + selectedCustomerData[18]);
+        lblHType1.setText(selectedCustomerData[19]);
+        lblHBuildingMaterial1.setText(selectedCustomerData[20]);
+        lblHStandard1.setText(selectedCustomerData[21]);
+        lblHSquaremeter1.setText(selectedCustomerData[22]);
+        lblHInsuranceVB1.setText(selectedCustomerData[23]);
+        lblHInsuranceVC1.setText(selectedCustomerData[24]);
+
+        //"Båtforsikring" tab'en
+        lblBInsuranceNr1.setText(selectedCustomerData[25]);
+        lblBInsurancePrm1.setText(selectedCustomerData[26]);
+        lblBDateForInsurance2.setText(selectedCustomerData[27]);
+        lblBAmountInsurance1.setText(selectedCustomerData[28]);
+        lblBOwner1.setText(selectedCustomerData[29] + " " + selectedCustomerData[30]);
+        lblBRegNr1.setText(selectedCustomerData[31]);
+        lblBTypeModel1.setText(selectedCustomerData[32]);
+        lblHType11.setText(selectedCustomerData[33]);
+        lblBYear1.setText(selectedCustomerData[34]);
+        lblBMotor1.setText(selectedCustomerData[35]);
+
+        //"Reiseforsikring" tab'en
+        lblTInsuranceNr1.setText(selectedCustomerData[36]);
+        lblTInsurancePrm1.setText(selectedCustomerData[37]);
+        lblTDateForInsurance1.setText(selectedCustomerData[38]);
+        lblTAmountInsurance1.setText(selectedCustomerData[39]);
+        lblAreaInsurance1.setText(selectedCustomerData[40] + " " + selectedCustomerData[41] + " " + selectedCustomerData[42] + " " + selectedCustomerData[43] + " " + selectedCustomerData[44]);
+        lblTSumInsurance1.setText(selectedCustomerData[45]);
     }
 
     private String[] parseCustomerData(String data){
