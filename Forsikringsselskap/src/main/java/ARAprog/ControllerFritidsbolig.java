b8bbc131848e761bb2c3e49241ee16974818d028
ansibansi <anette.w.amundsen@gmail.com>
2019-05-02 16:02:43 +0200

    Gi meg kaffe

--- Forsikringsselskap/src/main/java/ARAprog/ControllerFritidsbolig.java
+++ Forsikringsselskap/src/main/java/ARAprog/ControllerFritidsbolig.java
@@ -1,9 +1,11 @@
 package ARAprog;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class ControllerFritidsbolig {
     @FXML
@@ -11,4 +13,43 @@ public class ControllerFritidsbolig {
     inputCMaterial, inputCAmountC;
     public CheckBox cPremiumCb, cAmountCb, cAddressCb, cTypeCb, cStandardCb, cSquaremeterCb, cAmountBCb, cAmountICb, cMaterialCb;
     public Button buttonOpprettNyFritidsB, buttonLukkOpprettNyFritidsB;
+
+    public void OpprettNyFritidsB (ActionEvent actionEvent){
+
+    }
+
+    public void verifyCpremium (KeyEvent keyEvent){
+
+    }
+
+    public void verifyCAddress (KeyEvent keyEvent){
+
+    }
+
+    public void verifyCAmount (KeyEvent keyEvent){
+
+    }
+
+    public void verifyCType (KeyEvent keyEvent){
+
+    }
+
+    public void verifyCStandard (KeyEvent keyEvent){
+
+    }
+    public void verifyHSquareM (KeyEvent keyEvent){
+
+    }
+    public void verifyHAmountB (KeyEvent keyEvent){
+
+    }
+    public void verifyCMaterial (KeyEvent keyEvent){
+
+    }
+    public void verifyCAmountC (KeyEvent keyEvent){
+
+    }
+    public void lukkOpprettNyFritidsB (ActionEvent actionEvent){
+
+    }
 }
